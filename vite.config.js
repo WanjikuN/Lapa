e335725739f632c3701526534c1730fc9146b321
pwanjiku <cikunjoroge4@gmail.com>
2024-10-04 09:29:19 +0300

    Initial commit

---
+++ vite.config.js
@@ -0,0 +1,24 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  optimizeDeps: {
+    include: ["@emotion/react", "@emotion/styled", "@mui/material/Tooltip"],
+  },
+  plugins: [
+    react({
+      jsxImportSource: "@emotion/react",
+      babel: {
+        plugins: ["@emotion/babel-plugin"],
+      },
+    }),
+  ],
+  plugins: [react()],
+  esbuild: {
+    supported: { "top-level-await": true },
+  },
+  server: {
+    port: 3001,
+  },
+});
