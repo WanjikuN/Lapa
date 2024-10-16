278ebf51bf8f571b664f5e850aa02ac29ad6d664
pwanjiku <cikunjoroge4@gmail.com>
2024-10-16 15:03:14 +0300

    Update Components in about page

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,19 +1,34 @@
+import React from "react";
 import FooterHome from "../components/FooterHome";
 import Navbar from "../components/Navbar";
 import OurCause from "./OurCause";
 import WorkWithUs from "./WorkWithUs";
 import "./About.css";
+import AboutUsMain from "./AboutUsMain";
 
 const AboutUs = ({ setPage, page }) => {
-
+  const renderContent = () => {
+    switch (page) {
+      case "OurCause":
+        return <OurCause />;
+      case "WorkWithUs":
+        return <WorkWithUs />;
+      case "AboutUs":
+        return <AboutUsMain />;
+      default:
+        return <AboutUsMain />;
+    }
+  };
 
   return (
     <div id="container">
-      {/* navbar */
-        <Navbar />
-        }
-      <div id="about-cont">{}</div>
-      <div>{/* footer */}
+      {/* Render the Navbar and pass setPage to update the page state */}
+      <Navbar setPage={setPage} />
+      <div id="about-cont">
+        {renderContent()} {/* Conditionally render based on page */}
+      </div>
+      <div>
+        {/* footer */}
         <FooterHome />
       </div>
     </div>
